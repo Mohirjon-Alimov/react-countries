94e623d2626137d418336867041805fe1140123a
Mohirjon <mohir7209@gmail.com>
2022-08-08 01:16:23 +0500

    bugs fixed

--- src/components/search.js
+++ src/components/search.js
@@ -12,9 +12,9 @@ export const Search = ({setValue}) => {
   return (
     <>
       <div className="searchBox d-flex align-items-center">
-        <form className="d-flex align-items-center" onSubmit={(evt) => catchInpValue(evt)}>
-          <input ref={elInput} className="searchInput position-relative" type="text" placeholder="Search for a country…" />
-          <button className="btn btn-primary mx-4" type="submit">Search</button>
+        <form className="d-sm-flex align-items-center" onSubmit={(evt) => catchInpValue(evt)}>
+          <input ref={elInput} className="searchInput position-relative" type="search" placeholder="Search for a country…" />
+          <button className="btn btn-primary mx-4 my-2" type="submit">Search</button>
         </form>
       </div>
     </>
